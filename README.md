5399c96048b84d6ee5542b6bf707067b402dc5b4
Andy Lok <andylokandy@hotmail.com>
2025-03-21 13:05:11 +0800

    chore: initialize

---
+++ README.md
@@ -0,0 +1,9 @@
+# FastLabs / Blog
+
+[fast.github.io](https://fast.github.io)
+
+FastLabs is a community of open source contributors focused on building high-quality Rust crates. We collaborate on various projects to enhance the Rust ecosystem and provide reliable tools for developers.
+
+## Thanks
+
+- [slate-blog](https://github.com/SlateDesign/slate-blog) - For providing the blog template
---
+++ slate.config.ts
@@ -0,0 +1,22 @@
+/*
+ * @file Theme configuration
+ */
+import { defineConfig } from './src/helpers/config-helper';
+
+export default defineConfig({
+  lang: 'en-US',
+  site: 'https://fast.github.io',
+  title: 'FlatLabs / Blog',
+  description: 'We develop fast Rust crates and release them fast.',
+  lastModified: true,
+  readTime: true,
+  footer: {
+    copyright: '© 2025 FastLabs',
+  },
+  socialLinks: [
+    {
+      icon: 'github',
+      link: 'https://github.com/fast'
+    },
+]
+});
